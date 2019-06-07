a7818c3d84cf951fa7ad0f6acbfe50957cfc2bd4
Louis Pouillot <louis.pouillot@dataiku.com>
2019-06-07 17:16:14 +0200

    Fix version comparaison

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -8,5 +8,21 @@ export function canCreateFolderAndSafelySave(dssVersion: string): boolean {
 
 function versionGreaterThan(currentDSSVersion: string, majorToCompare: number, minorToCompare: number, revisionToCompare: number) {
     let [ major, minor, revision ] = currentDSSVersion.split(".");
-    return Number(major) < majorToCompare || Number(minor) < minorToCompare || Number(revision) < revisionToCompare;
+    if (Number(major) < majorToCompare) {	   
+        return false;	
+    } else if (Number(major) > majorToCompare) {	
+        return true;	
+    } else {	
+        if (Number(minor) < minorToCompare) {	
+            return false;	
+        } else if (Number(minor) > minorToCompare) {	
+            return true;	
+        } else {
+            if (Number(revision) < revisionToCompare) {	
+                return false;	
+            } else {	
+                return true;	
+            }	
+        }
+     }
 }
